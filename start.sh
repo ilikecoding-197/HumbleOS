641156933fb7a91db0c5c9134d9e8064ab396e53
tom_tom0418 <64pigpig2014@gmail.com>
2024-08-31 17:24:29 -0400

    Try to add C...

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@ BUILD_DIR="build"
 ISO_DIR="$BUILD_DIR/iso"
 SRC_DIR="src"
 ASM_FILES=("multiboot_header" "boot")
+C_FILES=("main")
+GCC_ARGS="-ffreestanding -mgeneral-regs-only -Wall -Wextra -Werror -m32"
 LD_INPUT=""
 KERNEL="kernel.bin"
 LD_FILE="linker.ld"
@@ -13,6 +15,12 @@ for i in "${ASM_FILES[@]}"
 do
    LD_INPUT="$LD_INPUT $BUILD_DIR/$i.o"
    nasm -o $BUILD_DIR/$i.o $SRC_DIR/$i.asm -felf64
+done
+
+for i in "${C_FILES[@]}"
+do
+   LD_INPUT="$LD_INPUT $BUILD_DIR/$i.o"
+   gcc -o $BUILD_DIR/$i.o $SRC_DIR/$i.c $GCC_ARGS
 done
 
 ld $LD_ARGS $LD_INPUT

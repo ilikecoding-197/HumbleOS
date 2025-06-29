abfede07e4b80224da8cabc5fdcad0e82bdb630b
Thomas <64pigpig2014@gmail.com>
2025-06-29 16:02:13 -0400

    cpuid - print the actual features

--- src/include/cpuid.h
+++ src/include/cpuid.h
@@ -4,8 +4,23 @@
 #ifndef CPUID_H
 #define CPUID_H
 
+#include <ints.h>
+
 /// @brief Initalize CPUID
 /// @note  If CPUID is not supported, a PANIC will happen.
 void cpuid_init();
+
+// CPUID features
+typedef enum {
+    SSE3, PCLMUL, DTES64, MONITOR, DS_CPL, VMX, SMX, EST, TM2, SSSE3, CID, SDBG, FMA, CX16, XTPR, PDCM, PCID,
+    DCA, SSE4_1, SSE4_2, X2APIC, MOVBE, POPCNT, TSC1, AES, XSAVE, OSXSAVE, AVX, F16C, RDRAND, HYPERVISOR, VME,
+    DE, PSE, TSC2, MSR, PAE, MCE, CX8, APIC, SEP, MTRR, PGE, MCA, CMOV, PAT, PSE36, PSN, CLFLUSH, DS, ACPI, MMX,
+    FXSR, SSE, SSE2, SS, HTT, TM, IA64, PBE
+} cpuid_feat;
+
+/// @brief Get CPUID feature
+/// @param feat Feature to get.
+/// @return 1 if the feature is supported, 0 otherwise.
+u8 cpuid_get_feat(cpuid_feat feat);
 
 #endif // CPUID_H
--- src/lib/cpuid.c
+++ src/lib/cpuid.c
@@ -11,17 +11,104 @@ u32 _cpuid_feat_ecx;
 u32 _cpuid_feat_edx;
 extern u8 _cpuid_supported;
 
-void cpuid_init() {
+struct {
+    cpuid_feat feat;
+    char *text;
+} cpuid_feat_texts[] = {
+    {SSE3, "SSE3"},
+    {PCLMUL, "PCLMUL"},
+    {DTES64, "DTES64"},
+    {MONITOR, "MONITOR"},
+    {DS_CPL, "DS_CPL"},
+    {VMX, "VMX"},
+    {SMX, "SMX"},
+    {EST, "EST"},
+    {TM2, "TM2"},
+    {SSSE3, "SSSE3"},
+    {CID, "CID"},
+    {SDBG, "SDBG"},
+    {FMA, "FMA"},
+    {CX16, "CX16"},
+    {XTPR, "XTPR"},
+    {PDCM, "PDCM"},
+    {PCID, "PCID"},
+    {DCA, "DCA"},
+    {SSE4_1, "SSE4_1"},
+    {SSE4_2, "SSE4_2"},
+    {X2APIC, "X2APIC"},
+    {MOVBE, "MOVBE"},
+    {POPCNT, "POPCNT"},
+    {TSC1, "TSC"},
+    {AES, "AES"},
+    {XSAVE, "XSAVE"},
+    {OSXSAVE, "OSXSAVE"},
+    {AVX, "AVX"},
+    {F16C, "F16C"},
+    {RDRAND, "RDRAND"},
+    {HYPERVISOR, "HYPERVISOR"},
+    {VME, "VME"},
+    {DE, "DE"},
+    {PSE, "PSE"},
+    {TSC2, "TSC"},
+    {MSR, "MSR"},
+    {PAE, "PAE"},
+    {MCE, "MCE"},
+    {CX8, "CX8"},
+    {APIC, "APIC"},
+    {SEP, "SEP"},
+    {MTRR, "MTRR"},
+    {PGE, "PGE"},
+    {MCA, "MCA"},
+    {CMOV, "CMOV"},
+    {PAT, "PAT"},
+    {PSE36, "PSE36"},
+    {PSN, "PSN"},
+    {CLFLUSH, "CLFLUSH"},
+    {DS, "DS"},
+    {ACPI, "ACPI"},
+    {MMX, "MMX"},
+    {FXSR, "FXSR"},
+    {SSE, "SSE"},
+    {SSE2, "SSE2"},
+    {SS, "SS"},
+    {HTT, "HTT"},
+    {TM, "TM"},
+    {IA64, "IA64"},
+    {PBE, "PBE"},
+};
+
+void cpuid_init()
+{
     klog("CPUID", "checking for cpuid...");
-    __asm__ ("call _cpuid_check"); // Check for CPUID
-    if (!_cpuid_supported) {
+    __asm__("call _cpuid_check"); // Check for CPUID
+    if (!_cpuid_supported)
+    {
         PANIC("CPUID NOT SUPPPORTED");
     }
-    
+
     klog("CPUID", "cpuid is supported, getting features...");
-    __asm__ ("call _cpuid_get_features");
+    __asm__("call _cpuid_get_features");
     klog("CPUID", "features gotten.");
 
     klogf("CPUID", "ECX: %x", _cpuid_feat_ecx);
     klogf("CPUID", "EDX: %x", _cpuid_feat_edx);
+
+    klog_prefix("CPUID");
+    print("that means this processor supports the following features: ");
+    for (int i = 0; i < sizeof(cpuid_feat_texts) / sizeof(cpuid_feat_texts[0]); i++) {
+        if (cpuid_get_feat(cpuid_feat_texts[i].feat)) {
+            print(cpuid_feat_texts[i].text);
+            print(" ");
+        }
+    }
+    print("\n");
+}
+
+
+u8 cpuid_get_feat(cpuid_feat feat) {
+    if (feat < 32) {
+        return (_cpuid_feat_ecx >> feat) & 1;
+    } else {
+        return (_cpuid_feat_edx >> (feat - 32)) & 1;
+    }
 }

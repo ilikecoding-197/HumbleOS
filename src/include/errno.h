f0530bee9a406cb38dfab600b05e7740be997915
Thomas Shrader <64pigpig2014@gmail.com>
2025-09-13 12:54:50 -0400

    errno

---
+++ src/include/errno.h
@@ -0,0 +1,39 @@
+/*
+    errno.h - errno header
+
+    Part of HumbleOS
+
+    Copyright 2025 Thomas Shrader
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+    and associated documentation files (the “Software”), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge, publish, distribute,
+    sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial
+    portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+    NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+#ifndef ERRNO_H
+#define ERRNO_H
+
+// errno values
+typedef enum {
+    EOK, // no error
+} errno_t;
+
+// global errno
+extern errno_t errno;
+
+// get the current errno and convert it a string, using a lookup table under the hood.
+// PLEASE do not modify the returned value!
+const char *strerror();
+
+#endif /* ERRNO_H */
